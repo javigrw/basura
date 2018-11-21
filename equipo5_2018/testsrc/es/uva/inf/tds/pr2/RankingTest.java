d49e0523fa646028ac5f1196b72e7bcfee9ef103
raumedi <raul.medina@alumnos.uva.es>
2018-11-21 22:13:35 +0100

    creados metodos de test sin definir para Ranking

--- equipo5_2018/testsrc/es/uva/inf/tds/pr2/RankingTest.java
+++ equipo5_2018/testsrc/es/uva/inf/tds/pr2/RankingTest.java
@@ -7,7 +7,50 @@ import org.junit.Test;
 public class RankingTest {
 
 	@Test
-	public void test() {
+	public void testDefaultRanking() {
+		//TODO
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testCustomRanking() {
+		//TODO
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testGetElementByPosition() {
+		//TODO
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testGetPositionFromElementValue() {
+		//TODO
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testGetPositionFromElementReference() {
+		//TODO
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testExistsElementValue() {
+		//TODO
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testExistsElementReference() {
+		//TODO
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testCompareWith() {
+		//TODO
 		fail("Not yet implemented");
 	}
 
