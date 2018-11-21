4da328b567623e7841995576c22bf1d74b6e4c28
raumedi <raul.medina@alumnos.uva.es>
2018-11-21 22:16:27 +0100

    definida funcionalidad para la creacion de un Ranking por defecto en el javadoc

--- equipo5_2018/src/es/uva/inf/tds/pr2/Ranking.java
+++ equipo5_2018/src/es/uva/inf/tds/pr2/Ranking.java
@@ -1,5 +1,17 @@
 package es.uva.inf.tds.pr2;
 
-public class Ranking {
+public class Ranking<Element> {
 
+	protected Element[] top;
+	
+	/**
+	 * Creates a top 10 ranking from an array of elements.
+	 * The order in the array will match the positions at the ranking.
+	 * rankData[0] will be top 1
+	 * rankData[9] will be top 10
+	 * @param rankData array with size of 10
+	 */
+	Ranking(Element[] rankData){
+		//TODO not implemented yet
+	}
 }
