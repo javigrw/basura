16fa6367a7ff47d49c2aa0b2cd978dfc9dcb4a36
raumedi <raul.medina@alumnos.uva.es>
2018-11-22 09:04:37 +0100

    definido test para obtencion de posicion por referencia de un elemento

--- equipo5_2018/testsrc/es/uva/inf/tds/pr2/RankingTest.java
+++ equipo5_2018/testsrc/es/uva/inf/tds/pr2/RankingTest.java
@@ -5,70 +5,79 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class RankingTest {
-	
+
 	@Test
 	public void testDefaultRanking() {
-		String[] rankingElements = {"top1","top2","top3","top4","top5","top6","top7","top8","top9","top10"};
-		Ranking<String> ranking= new Ranking<>(rankingElements);
-		
+		String[] rankingElements = { "top1", "top2", "top3", "top4", "top5", "top6", "top7", "top8", "top9", "top10" };
+		Ranking<String> ranking = new Ranking<>(rankingElements);
+
 		assertNotNull(ranking.top);
 		assertArrayEquals(rankingElements, ranking.top);
-		
+
 	}
-	
+
 	@Test
 	public void testCustomRanking() {
 		int size = 5;
-		String[] rankingElements = {"top1", "top2", "top3", "top4", "top5"};
+		String[] rankingElements = { "top1", "top2", "top3", "top4", "top5" };
 		Ranking<String> ranking = new Ranking<>(rankingElements, size);
-		
+
 		assertNotNull(ranking.top);
-		assertArrayEquals(rankingElements,ranking.top);
+		assertArrayEquals(rankingElements, ranking.top);
 	}
-	
+
 	@Test
 	public void testGetElementByPosition() {
 		int position = 4;
-		String[] rankingElements = {"top1","top2","top3","top4","top5","top6","top7","top8","top9","top10"};
+		String[] rankingElements = { "top1", "top2", "top3", "top4", "top5", "top6", "top7", "top8", "top9", "top10" };
 		Ranking<String> ranking = new Ranking<>(rankingElements);
-		
+
 		String element = ranking.getElementByPosition(position);
-		
+
 		assertEquals(rankingElements[3], element);
 	}
-	
+
 	@Test
 	public void testGetPositionFromElementValue() {
 		int expectedPosition = 4;
-		String[] rankingElements = {"top1","top2","top3","top4","top5","top6","top7","top8","top9","top10"};
+		String[] rankingElements = { "top1", "top2", "top3", "top4", "top5", "top6", "top7", "top8", "top9", "top10" };
 		Ranking<String> ranking = new Ranking<>(rankingElements);
-		
+
 		int position = ranking.getPositionFromElementValue("top4");
-		
+
 		assertEquals(expectedPosition, position);
 	}
-	
+
 	@Test
 	public void testGetPositionFromElementReference() {
-		//TODO
-		fail("Not yet implemented");
+		int expectedPosition = 4;
+		String expectedElement = "element";
+		String[] rankingElements = { "top1", "top2", "top3", expectedElement, "element", "top6", "top7", "top8", "top9",
+				"top10" };
+		Ranking<String> ranking = new Ranking<>(rankingElements);
+
+		int position = ranking.getPositionFromElementReference(expectedElement);
+		String element = ranking.getElementByPosition(position);
+
+		assertSame(expectedElement, element);
+		assertEquals(expectedPosition, position);
 	}
-	
+
 	@Test
 	public void testExistsElementValue() {
-		//TODO
+		// TODO
 		fail("Not yet implemented");
 	}
-	
+
 	@Test
 	public void testExistsElementReference() {
-		//TODO
+		// TODO
 		fail("Not yet implemented");
 	}
-	
+
 	@Test
 	public void testCompareWith() {
-		//TODO
+		// TODO
 		fail("Not yet implemented");
 	}
 }
