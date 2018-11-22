a588dd150ec07de2f8311fb296dc67d86b9a222e
raumedi <raul.medina@alumnos.uva.es>
2018-11-22 09:09:44 +0100

    añadida definicion para la obtencion de posicion por referencia de un elemento (javadoc)

--- equipo5_2018/src/es/uva/inf/tds/pr2/Ranking.java
+++ equipo5_2018/src/es/uva/inf/tds/pr2/Ranking.java
@@ -3,32 +3,36 @@ package es.uva.inf.tds.pr2;
 public class Ranking<Element> {
 
 	protected Element[] top;
-	
+
 	/**
-	 * Creates a top 10 ranking from an array of elements.
-	 * The order in the array will match the positions at the ranking.
-	 * rankData[0] will be top 1
+	 * Creates a top 10 ranking from an array of elements. The order in the array
+	 * will match the positions at the ranking. rankData[0] will be top 1
 	 * rankData[9] will be top 10
-	 * @param rankData array with size of 10
+	 * 
+	 * @param rankData
+	 *            array with size of 10
 	 */
-	public Ranking(Element[] rankData){
-		//TODO not implemented yet
+	public Ranking(Element[] rankData) {
+		// TODO not implemented yet
 	}
-	
+
 	/**
-	 * Creates a top ranking with variable size from an array of elements.
-	 * The order in the array will match the position at the ranking.
-	 * rankData[0] will be top 1
-	 * rankData[9] will be top 10
-	 * @param rankData array of elements
-	 * @param topSize	can be equal or bigger than rankData size
+	 * Creates a top ranking with variable size from an array of elements. The order
+	 * in the array will match the position at the ranking. rankData[0] will be top
+	 * 1 rankData[9] will be top 10
+	 * 
+	 * @param rankData
+	 *            array of elements
+	 * @param topSize
+	 *            can be equal or bigger than rankData size
 	 */
-	public Ranking(Element[] rankData, int topSize){
-		//TODO not implemented yet
+	public Ranking(Element[] rankData, int topSize) {
+		// TODO not implemented yet
 	}
-	
+
 	/**
 	 * Returns the element at the ranking position desired
+	 * 
 	 * @param position
 	 * @return element at the ranking position
 	 */
@@ -39,10 +43,22 @@ public class Ranking<Element> {
 
 	/**
 	 * Returns the position of the element
+	 * 
 	 * @param element
-	 * @return position	the position of the element at the ranking
+	 * @return position the position of the element at the ranking
 	 */
 	public int getPositionFromElementValue(Element element) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/**
+	 * Returns the position of the element
+	 * 
+	 * @param element
+	 * @return position the position of the element at the ranking
+	 */
+	public int getPositionFromElementReference(Element element) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
