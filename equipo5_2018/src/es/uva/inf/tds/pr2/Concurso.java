d5ae17c5f5707c4e1ea16f073cf3b2ce719f4586
raumedi <raul.medina@alumnos.uva.es>
2018-11-28 13:55:06 +0100

    creada descripción para la clase y el metodo de creación

--- equipo5_2018/src/es/uva/inf/tds/pr2/Concurso.java
+++ equipo5_2018/src/es/uva/inf/tds/pr2/Concurso.java
@@ -1,5 +1,25 @@
 package es.uva.inf.tds.pr2;
 
-public class Concurso {
+/**
+ * Concurso of elements of the same type. There is a bottom limit of votes and
+ * nominations, so is a top limit of nominated elements. Bottom limit is 1/3 of
+ * top limit.
+ * 
+ * @author raumedi
+ *
+ * @param <Element>
+ */
+public class Concurso<Element> {
+	protected int nominationsLimit;
+	protected int votesLimit;
 
+	/**
+	 * Creates a new Concurso with a top limit of nominated elements and a botton
+	 * limit of elements needed to close a nomination phase and voting phase.
+	 * 
+	 * @param nominationsLimit
+	 */
+	public Concurso(int nominationsLimit) {
+		// TODO not implemented yet
+	}
 }
