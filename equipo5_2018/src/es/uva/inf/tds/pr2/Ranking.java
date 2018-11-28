b59355de16864206f948bb4ee03239a810893698
raumedi <raul.medina@alumnos.uva.es>
2018-11-28 11:42:43 +0100

    actualizada descripción para la creación con menos elementos que el tamaño del ranking

--- equipo5_2018/src/es/uva/inf/tds/pr2/Ranking.java
+++ equipo5_2018/src/es/uva/inf/tds/pr2/Ranking.java
@@ -7,7 +7,8 @@ public class Ranking<Element> {
 	/**
 	 * Creates a top 10 ranking from an array of elements. The order in the array
 	 * will match the positions at the ranking. rankData[0] will be top 1
-	 * rankData[9] will be top 10
+	 * rankData[9] will be top 10. If there are less elements than 10, the ranking
+	 * will be filled with null elements
 	 * 
 	 * @param rankData
 	 *            array with size of 10
@@ -19,7 +20,8 @@ public class Ranking<Element> {
 	/**
 	 * Creates a top ranking with variable size from an array of elements. The order
 	 * in the array will match the position at the ranking. rankData[0] will be top
-	 * 1 rankData[9] will be top 10
+	 * 1 rankData[9] will be top 10. If there are less elements than topSize, the
+	 * ranking will be filled with null elements
 	 * 
 	 * @param rankData
 	 *            array of elements
