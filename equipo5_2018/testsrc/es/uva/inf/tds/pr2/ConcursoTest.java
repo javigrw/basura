d3b6bd2b510abff0e92251a21a0656eb907815b7
raumedi <raul.medina@alumnos.uva.es>
2018-11-29 13:57:17 +0100

    añadida category para los test TDD

--- equipo5_2018/testsrc/es/uva/inf/tds/pr2/ConcursoTest.java
+++ equipo5_2018/testsrc/es/uva/inf/tds/pr2/ConcursoTest.java
@@ -2,8 +2,11 @@ package es.uva.inf.tds.pr2;
 
 import static org.junit.Assert.*;
 
-import org.junit.Test;
 
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+@Category(TDD.class)
 public class ConcursoTest {
 
 	@Test
