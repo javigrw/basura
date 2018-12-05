2df676dbc72f00f6621bc46ee9276e81be64e0c1
javguti <javigutrod@gmail.com>
2018-12-05 22:37:10 +0100

    creados test de secuencia

---
+++ equipo5_2018/testsrc/es/uva/inf/tds/pr2/SequenceTestRanking.java
@@ -0,0 +1,46 @@
+package es.uva.inf.tds.pr2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+import org.junit.experimental.categories.Category;
+
+@Category(Sequence.class)
+public class SequenceTestRanking {
+
+	@Test
+	public void test() {
+		
+		String[] topos = new String[]{"raulillo","bimbas","ase el humilde","antooro","agapito","papito mi rey","yorx","cachi"};
+		
+		Ranking<String> rank = new Ranking<>(topos, 3);
+		
+		assertNotNull(rank);
+		assertEquals(3,rank.top.length);
+		for(String topo : topos) {
+			assertTrue(rank.existsValue(topo));
+		}
+		
+		String[] otrosTopos = new String[]{"antooro","agapito","papito mi rey","yorx","raulillo","bimbas","ase el humilde","cachi"};
+		Ranking<String> rank2 = new Ranking<>(otrosTopos, 3);
+		
+		
+		assertEquals(1,rank.getPositionFromElementValue("raulillo"));
+		
+		int[] diff = rank.compareWith(rank2);
+		assertNotNull(diff);
+		
+		int[] realDiff = new int[] {4,4,4,-3,-3,-3,-3,0};
+		
+		for(int i = 0; i < realDiff.length; i++) {
+			
+			assertEquals(realDiff[i], diff[i]);
+			
+		}
+		
+		
+	}
+
+}
